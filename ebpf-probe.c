fa43d986fad4d174cf6632c1ba85117a2e0f6de8
blackdragoon26 <sankalp.jha9643@gmail.com>
2025-04-15 06:27:31 +0530

    improved kernel listener with the feature of filename process

--- ebpf-probe.c
+++ ebpf-probe.c
@@ -1,18 +1,28 @@
 #include <uapi/linux/ptrace.h>
 #include <linux/sched.h>
+#include <linux/fs.h>
 #include <bcc/proto.h>
 
-struct data_t{
+struct clone_data_t{
 	u32 pid;
 	u32 ppid;
 	char comm[TASK_COMM_LEN];
 };
 
-BPF_PERF_OUTPUT(events);
+struct open_data_t{
+	u32 pid;
+	u32 ppid;
+	u64 timestamp;
+	char comm[TASK_COMM_LEN];
+	char filename[NAME_MAX];
+};
+
+BPF_PERF_OUTPUT(clone_events);
+BPF_PERF_OUTPUT(open_events);
 
 
 int kprobe__sys_clone(void*ctx){ //ctx-> current pointer to thhis event
-	struct data_t data={};
+	struct clone_data_t data={};
 	struct task_struct*task;
 
 	task=(struct task_struct*)bpf_get_current_task();
@@ -20,8 +30,21 @@ int kprobe__sys_clone(void*ctx){ //ctx-> current pointer to thhis event
 	data.ppid=task->real_parent->tgid;
 	bpf_get_current_comm(&data.comm,sizeof(data.comm));
 
-	events.perf_submit(ctx,&data,sizeof(data));
+	clone_events.perf_submit(ctx,&data,sizeof(data));
 
 	return 0;
 
+}
+
+int kprobe__do_sys_openat2(struct pt_regs*ctx,int dfd,const char __user *filename,int flags,umode_t mode){
+	struct open_data_t data={};
+	
+	data.pid=bpf_get_current_pid_tgid();
+	data.timestamp=bpf_ktime_get_ns();
+	bpf_get_current_comm(&data.comm,sizeof(data.comm));
+
+	bpf_probe_read_user_str(&data.filename,sizeof(data.filename),filename);
+	open_events.perf_submit(ctx,&data,sizeof(data));
+
+	return 0;
 }
